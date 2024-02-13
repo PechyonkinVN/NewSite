ad4d21a57319727b2bb7a6463f4a81414e12c97f
Satis_Android <pvnn.22@uni-dubna.ru>
2024-02-13 22:23:55 +0300

    Исправление ошибки кривой ссылки на пони

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@ function checkTheme(page) {
     if (localStorage.getItem('theme_id') === null)
     {
         localStorage.setItem('theme_id', 'light');
-        localStorage.setItem('theme_url', "../Resources/Images/Selestia.png")
+        localStorage.setItem('theme_url', "Resources/Images/Selestia.png")
         console.log("Storage of Theme, hadn't been found. Create new.")
     }
     else
@@ -30,7 +30,7 @@ function changeTheme(img) {
     else
     {
         localStorage.setItem('theme_id', 'light');
-        localStorage.setItem('theme_url', "../Resources/Images/Selestia.png")
+        localStorage.setItem('theme_url', "Resources/Images/Selestia.png")
         img.src = "../Resources/Images/Selestia.png";
         img.id ="light";
     }
